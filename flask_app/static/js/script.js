09f8886c9802f0cdadad88ac3920ae0914687586
Brandon Colangelo <brandoncancode@gmail.com>
2023-09-29 13:53:35 -0400

    Working on about me animations.

---
+++ flask_app/static/js/script.js
@@ -0,0 +1,54 @@
+// Main banner
+// Career title animation
+var typed = new Typed(".auto-type", {
+    strings: ["Software Engineer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed:150,
+    loop: true
+})
+// End of main banner
+
+// About me
+// Change spanned text color
+var python = document.getElementById("python");
+var learn = document.getElementById("learn");
+var secure = document.getElementById("secure");
+var color = ["black", "white"];
+var i = 0;
+var currentElement = null;
+
+function change() {
+    if (currentElement !== null) {
+        currentElement.style.color = "black";
+    }
+
+    currentElement = (currentElement === null) ? secure :
+                        (currentElement === secure) ? learn :
+                        (currentElement === learn) ? python : secure;
+
+    currentElement.style.color = color[i];
+    i++;
+
+    if (i > color.length - 1) {
+        i = 0;
+    }
+}
+
+setInterval(change, 750);
+
+// Slideshow
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+var i;
+var x = document.getElementsByClassName("my_slides");
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+}
+slideIndex++;
+if (slideIndex > x.length) {slideIndex = 1}
+x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2500); // Change image every 2.5 seconds
+}
+// End of about me
