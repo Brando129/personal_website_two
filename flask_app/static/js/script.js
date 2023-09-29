3661d9b6e6ddbc71ba2d69a9fe4466d270fc3084
Brandon Colangelo <brandoncancode@gmail.com>
2023-09-29 18:04:55 -0400

    Responsiveness and animations.

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -49,6 +49,6 @@ for (i = 0; i < x.length; i++) {
 slideIndex++;
 if (slideIndex > x.length) {slideIndex = 1}
 x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2500); // Change image every 2.5 seconds
+  setTimeout(carousel, 3000); // Change image every 2.5 seconds
 }
 // End of about me
